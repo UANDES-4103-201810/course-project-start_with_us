7bb7d92771724cc2f00651081d3829daa8194491
TGRaydas <pgrand@miuandes.cl>
2018-05-01 20:54:08 -0300

    Devise Full Implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
-  belongs_to :role
+  before_create :default_role
+  has_one :role
   has_one :profile
   has_many :projects
   accepts_nested_attributes_for :profile
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def default_role
+    self.role_id = 1;
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, path: 'users', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' },
+             controllers: {sign_up: 'users/registrations'}
   resources :projects
   get '/', to: "home#index"
   get 'new_user', to: "user#new_user"
